fc81c11871b52ef4dcdcbdcbaa18ea7773cbd058
AsmaAlfauri <asmaa.alfauri@gmail.com>
2019-12-08 16:12:11 +0200

    day 14 exercises

---
+++ day 14/day 14 ex/day 14-eX.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  8 12:08:54 2019
+
+@author: Asma Alfauri
+"""
+
+# =============================================================================
+# #Exercise 1:
+# from flask import Flask
+# app=Flask(__name__)
+# 
+# @app.route('/')
+# def index():
+#     return 'This is the Index page'
+# 
+# @app.route('/hello')
+# def hello():
+#     return "Hello World!"
+# 
+# 
+# @app.route('/members')
+# def member():
+#     return "Members page"
+# 
+# 
+# if __name__=='__main__':
+#     app.run()
+# 
+# =============================================================================
+
+# =============================================================================
+# #Exercise 2:
+# from flask import Flask,render_template
+# app=Flask(__name__)
+#
+# @app.route('/index/<int:marks>')
+# def hello(marks):
+#     return render_template('Index.html',marks=marks)
+#
+#
+# if __name__=='__main__':
+#     app.run()
+# 
+# =============================================================================
+
+# =============================================================================
+# #Exercise 3:
+# from flask import Flask, render_template
+# app=Flask(__name__)
+# @app.route('/index')
+# def index():
+#     return render_template('index2.html')
+# if __name__=='__main__':
+#     app.run()
+# 
+# =============================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
